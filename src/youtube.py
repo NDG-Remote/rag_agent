912ecfe6bc6f3ff0d1e31747494565fe17d7ef6c
NDG-Remote <andrea.glaus.digital@gmail.com>
2025-03-10 10:53:16 +0100

    feat: add youtube tool for extracting oficial trailer video link

---
+++ src/youtube.py
@@ -0,0 +1,9 @@
+from langchain_community.tools import YouTubeSearchTool
+from name_filter import extract_name
+
+name = extract_name("When did the movie The God Father come out?")
+
+tool = YouTubeSearchTool()
+
+def extract_youtube_link(name: str) -> str:
+    return tool.run(f"{name} oficial trailer,1")
